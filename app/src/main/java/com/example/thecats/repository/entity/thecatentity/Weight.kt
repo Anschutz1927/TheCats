f711fdf7bdefc48c80148d018c0318901ac55831
Yauheni S <blackpearl_2007@tut.by>
2020-11-26 14:21:24 +0300

    Removed old module

--- app/src/main/java/com/example/thecats/repository/entity/thecatentity/Weight.kt
+++
@@ -1,6 +0,0 @@
-package com.example.thecats.repository.entity.thecatentity
-
-data class Weight(
-    val imperial: String,
-    val metric: String
-)
--- app/src/main/java/com/example/thecats/utils/ActivityUtils.kt
+++
@@ -1,3 +0,0 @@
-package com.example.thecats.utils
-
-enum class FragmentType { MAIN, FAVORITES }
